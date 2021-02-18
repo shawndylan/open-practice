4e5740b1eecf1ad60e9734aeaf43a610e815d582
shawndylan <dylanjohnston17@gmail.com>
2021-02-18 10:42:26 -0500

    Updates

---
+++ precache-manifest.363517a990e0173de16c41c4a37af642.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8816de9be1ece9f52994f34cdc401c8e",
+    "url": "/open-practice/index.html"
+  },
+  {
+    "revision": "c05028c43ed175f2abb2",
+    "url": "/open-practice/static/css/2.8b86c1cf.chunk.css"
+  },
+  {
+    "revision": "12fa58d22531016baa5c",
+    "url": "/open-practice/static/css/main.6c39bc14.chunk.css"
+  },
+  {
+    "revision": "c05028c43ed175f2abb2",
+    "url": "/open-practice/static/js/2.74a77706.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/open-practice/static/js/2.74a77706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12fa58d22531016baa5c",
+    "url": "/open-practice/static/js/main.5076e519.chunk.js"
+  },
+  {
+    "revision": "1ed679450b5767b1cd4d",
+    "url": "/open-practice/static/js/runtime-main.cded3c57.js"
+  },
+  {
+    "revision": "401fdae3ad10a52561c3e4a8a787c7fc",
+    "url": "/open-practice/static/media/CollectiveStructure.401fdae3.svg"
+  },
+  {
+    "revision": "94b7fa750d8e6edac330e41d7d0e69c0",
+    "url": "/open-practice/static/media/CooperativeStructure.94b7fa75.svg"
+  },
+  {
+    "revision": "cc84e350698e51d02bc04322f84559af",
+    "url": "/open-practice/static/media/NetworkStructure.cc84e350.svg"
+  },
+  {
+    "revision": "f558c69289f49f8c2439f419ae2c882e",
+    "url": "/open-practice/static/media/NonProfitStructure.f558c692.svg"
+  },
+  {
+    "revision": "1ff38cc293ed6ff2606a57ef1acce03e",
+    "url": "/open-practice/static/media/PlatformStructure.1ff38cc2.svg"
+  },
+  {
+    "revision": "bda7ac3079458189ce67140959836d1a",
+    "url": "/open-practice/static/media/PracticeStructures.bda7ac30.svg"
+  }
+]);
